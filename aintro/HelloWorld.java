5bd8c425b8c3fe35f00fd728afc4cbfc93409ab1
Allan Andrew da Cruz <allan.andrewdc@gmail.com>
2023-07-02 21:14:43 -0300

    first commit

---
+++ aintro/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.cursojava.aintro;
+
+public class HelloWorld { // a criação de classes é CamelCase
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
+
