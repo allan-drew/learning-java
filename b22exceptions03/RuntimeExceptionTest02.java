cd48d907899221e8182193295c1a201451f1d0a2
Allan Andrew da Cruz <allan.andrewdc@gmail.com>
2023-08-13 16:58:37 -0300

    runtime exception, arithmetic exception

--- b22exceptions03/RuntimeExceptionTest02.java
+++ b22exceptions03/RuntimeExceptionTest02.java
@@ -1,2 +1,22 @@
-package br.com.cursojava.b22exceptions03;public class RuntimeExceptionTest02 {
+package br.com.cursojava.b22exceptions03;
+
+public class RuntimeExceptionTest02 {
+
+    public static void main(String[] args) {
+        System.out.println("Resultado: ");
+        System.out.println(dividir(3, 0));
+    }
+
+
+    private static int dividir(int a, int b) {
+
+        try {
+            return a/b;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        return 0; // retorna 0 se o catch executar
+
+    }
+
 }
