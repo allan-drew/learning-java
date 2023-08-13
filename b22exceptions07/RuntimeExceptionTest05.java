953ef3295505a9a9f47f34df3dd169d5a8e1c265
Allan Andrew da Cruz <allan.andrewdc@gmail.com>
2023-08-13 17:31:40 -0300

    multiple exceptions

--- b22exceptions07/RuntimeExceptionTest05.java
+++ b22exceptions07/RuntimeExceptionTest05.java
@@ -1,2 +1,26 @@
-package br.com.cursojava.b22exceptions07;public class RuntimeExceptionTest05 {
+package br.com.cursojava.b22exceptions07;
+
+// Capturando múltiplas exceções
+// Exceções mais genéricas vão no final dos catchs
+
+public class RuntimeExceptionTest05 {
+
+    public static void main(String[] args) {
+
+        try {
+            throw new IllegalArgumentException();
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("excecao ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("excecao IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("excecao IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("excecao ArithmeticException");
+        } catch (RuntimeException e) { // Exceções mais genéricas vão no final dos catchs
+            System.out.println("excecao RuntimeException");
+        }
+
+    }
+
 }
