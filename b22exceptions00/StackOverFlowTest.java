900ff12c69579c72b03dbed8c6a7edd3ce527c4b
Allan Andrew da Cruz <allan.andrewdc@gmail.com>
2023-08-13 16:26:41 -0300

    exception test stackoverflow

--- b22exceptions00/StackOverFlowTest.java
+++ b22exceptions00/StackOverFlowTest.java
@@ -1,6 +1,19 @@
-package br.com.cursojava.b22exceptions00.test;
+package br.com.cursojava.b22exceptions00;
 
 public class StackOverFlowTest {
+
+    /*
+     * Error é filha de Throwable. Logo, Error não é filha de Exception.
+     * Com isso, Error não é Exception.
+     * */
+
+    /*
+    * Error: situações que não conseguimos recuperar em tempo de execução.
+    * Logo, precisamos parar o programa, resolver o código e executar novamente.
+    *
+    * Exemplo: stack overflow error
+    * Error extende de Throwable
+    * */
 
     public static void main(String[] args) {
         recursividade();
