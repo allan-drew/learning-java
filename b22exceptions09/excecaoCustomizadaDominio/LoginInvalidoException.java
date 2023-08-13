b90d215a00ed6f045252e53b1c7c9fff5ef9c068
Allan Andrew da Cruz <allan.andrewdc@gmail.com>
2023-08-13 17:37:41 -0300

    excecoes customizadas

--- b22exceptions09/excecaoCustomizadaDominio/LoginInvalidoException.java
+++ b22exceptions09/excecaoCustomizadaDominio/LoginInvalidoException.java
@@ -1,2 +1,17 @@
-package br.com.cursojava.b22exceptions09.excecaoCustomizadaDominio;public class LoginInvalidoException {
+package br.com.cursojava.b22exceptions09.excecaoCustomizadaDominio;
+
+
+// Podemos criar excecoes customizadas
+// O tratamento dessas excecoes customizadas pode ser definido de acordo com a lógica de negócio do sistema
+
+public class LoginInvalidoException extends Exception {
+
+    public LoginInvalidoException() {
+        System.out.println("Login inválido!");
+    }
+
+    public LoginInvalidoException(String message) {
+        super(message);
+    }
+
 }
--- b22exceptions09/excecaoCustomizadaTest/LoginInvalidoTest.java
+++ b22exceptions09/excecaoCustomizadaTest/LoginInvalidoTest.java
@@ -1,2 +1,39 @@
-package br.com.cursojava.b22exceptions09.excecaoCustomizadaTest;public class LoginInvalidoTest {
+package br.com.cursojava.b22exceptions09.excecaoCustomizadaTest;
+
+
+import br.com.cursojava.b22exceptions09.excecaoCustomizadaDominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoTest {
+
+    public static void main(String[] args) {
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logar() throws LoginInvalidoException {
+        //simulando
+        Scanner entradaTeclado = new Scanner(System.in);
+        String username01BancoDeDados = "Messi";
+        String senha01BancoDeDados = "argentina123";
+
+        System.out.println("Digite o usuário: ");
+        String usuarioDigitado = entradaTeclado.nextLine();
+        System.out.println("Digite a senha: ");
+        String senhaDigitada = entradaTeclado.nextLine();
+
+        if (!username01BancoDeDados.equals(usuarioDigitado) || !senha01BancoDeDados.equals(senhaDigitada)) {
+            throw new LoginInvalidoException("USUARIO OU SENHA INVÁLIDOS!");
+        }
+
+        System.out.println("Usuário logado!");
+
+    }
+
+
+
 }
