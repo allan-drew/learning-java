6fd2eff58d8d17ebf1ee8241b35f89d4e317aca6
Allan Andrew da Cruz <allan.andrewdc@gmail.com>
2023-08-13 16:29:18 -0300

    exception test runtime

--- b22exceptions01/RuntimeExceptionTest.java
+++ b22exceptions01/RuntimeExceptionTest.java
@@ -1,2 +1,21 @@
-package br.com.cursojava.b22exceptions01;public class RuntimeExceptionTest {
+package br.com.cursojava.b22exceptions01;
+
+public class RuntimeExceptionTest {
+
+    public static void main(String[] args) {
+
+        /* Unchecked exceptions: RuntimeException and its subclasses are unchecked exceptions.
+
+        * RuntimeException são exceções ocorridas por erro no desenvolvimento.
+        * RuntimeException é filha de Exception, que é filha de Throwable.
+
+        * https://docs.oracle.com/javase/8/docs/api/java/lang/RuntimeException.html
+        * */
+
+        // exemplo (ArrayIndexOutOfBoundsException):
+        int[] numeros = {1, 4, 2};
+        System.out.println(numeros[3]); // runtime exception --> ArrayIndexOutOfBoundsException
+
+    }
+
 }
